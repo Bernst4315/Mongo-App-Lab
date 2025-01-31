b0ae199e5e7d6bae5dc20c8279f791124e9e909f
bernst4315 <bernst4315@gmail.com>
2025-01-31 12:06:38 -0700

    created plants schema

---
+++ models/plants.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const plantSchema = new mongoose.Schema({
+    plant_code: {
+        type: String,
+        required: true, 
+    },
+
+    sci_name: {
+        type: String,
+        required: true, 
+    },
+
+    common_name: {
+        type: String,
+        required: false, 
+    },
+
+    notes: {
+        type: String,
+        required: false, 
+    },
+
+    state_habitat: {
+        type: String,
+        required: true, 
+    }
+
+})
+
+export default mongoose.model("plants", plantSchema)
